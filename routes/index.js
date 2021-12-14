b01c0fbbbae2e65724efcd7db22b99b1855f8182
Darrell Cole Hill <darrellcolehill@gmail.com>
2021-12-13 22:30:34 -0500

    test code for displaying inboxes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,81 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db').db;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Private Chat App' });
+  //res.render('index', { title: 'Private Chat App' });
+  var username = "username"; // TODO: CHANGE TO GET FROM COOKIE
+  var inboxes;
+  var user1 = req.query.user1;
+  var user2 = req.query.user2;
+  var messages;
+
+
+  db.query("SELECT * FROM inconversation WHERE username = ?", username, function(err, data)	{
+    if(err)
+    {
+      console.log(err);
+    }
+    else
+    {
+      inboxes = data;
+    }
+  });
+
+
+  // Checks is the user1 and user2 variables are defined in the URL,
+  // if so, get the messages between user1 and user2
+  if(user1 && user2)
+  {
+
+    var inbox = [user1, user2];
+
+    //var data = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+    // makes sure that the user's token matches to the user1 or user2 username values before displaying the messages
+    // Ensures that a user is not able to see another users messages
+    if(username == user1 || username == user2)
+    {
+      db.query("SELECT COUNT(*) AS cnt FROM inbox WHERE user1 = ? AND user2 = ?", inbox, function(err, data)  {
+
+        if(!err && data[0].cnt != 0)
+        {
+            // TODO: update query so that it only returns the last 10 messages OR just extract the last 10 items from the data object
+          db.query("SELECT content, sender, messageNo FROM message WHERE user1 = ? AND user2 = ?", inbox,  function(err, data) {
+            if(err)
+            {
+              console.log(err);
+            }
+            else
+            {
+              // Store current user as an index ot the inbox array
+              console.log(data);
+              res.render('index', { title: 'Private Chat App', messages: data, inboxes, curInbox: inbox, curUser: username});
+            }
+          });
+        }
+        else
+        {
+          console.log("SQL error or Requested inbox does not exist");
+          res.redirect("/login"); // TODO: send error status here
+        }
+      })
+    }
+    else
+    {
+      console.log("token or URL parameters is incorrect");
+      res.redirect("/login"); // TODO: send error status here
+    }
+  }
+  else
+  {
+    //console.log("MISSING USERSESERSERSERES");
+    res.render('index', { title: 'Private Chat App', inboxes: data});
+  }
+
+
+
 });
 
 module.exports = router;
