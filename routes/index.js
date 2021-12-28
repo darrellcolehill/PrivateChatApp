cddd13d2eea7094545b5023fe193d57ed8d364de
Darrell Cole Hill <darrellcolehill@gmail.com>
2021-12-28 10:04:48 -0500

    Added support for sending message with dummy token value
    Can now send messages with a dummy token (username) value. Need to make sure that I update the code to get the user's username from the token.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db').db;
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Private Chat App' });
@@ -78,24 +80,107 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/', function(req, res, next) {
-  //console.log("post made");
-  //res.render('index', { title: 'Private Chat App', inboxes, messages: null});
+
+// Message is sent
+router.post('/', function(req, res, next){
 
   var username = "username"; // TODO: CHANGE TO GET FROM COOKIE
-  var inboxes;
   var user1 = req.query.user1;
   var user2 = req.query.user2;
-  var messages;
+  var inbox = [user1, user2];
 
-  if(user1 && user2)
+  // grabs the message from the body 
+  var messageContent = req.body.message;
+
+  // Procedure for inserting message and updaating inbox
+  /*
+    pre) ensure that cookie exist and that cookie data corresponds to requested inbox data
+    1) Find the number of messages sent bewteen the two users
+    2) Insert the new message into the message DB (with the inbox keys specified in the URL parameters) by performming the following actions
+      a) set the user1 and user2 message attributes to the user1 and user2 variables specified in the URL
+      b) set the messageNo = numMessages + 1 (with numMessages being retrieved from step 1)
+      c) examine the users login cookie to determin who the sender is and make the other user the recipient
+      d) set the content = message (message is located in the body of the request)
+    3) Update the number of messges sent in the corresponding inbox (with the inbox keys specified in the URL parameters)
+  */
+
+  // Makes sure that the users tokin matches to the user1 or user2 username values before displaying the messages
+  //var token = req.cookies.whatsAppClone;
+  if(!username || !user1 || !user2 || !messageContent)
   {
-    // TODO: send message to specified inbox and then reidrect to the same page so the messages page is updated. 
+    console.log("token or message data does not exist");
+    //console.log(username + " " + user1 +  " " + user2 + " " + messageContent);
+    res.redirect("/login"); // TODO: send error status here
   }
   else
   {
-    // missing inbox data in url variables
+    try{
+      //var data = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+      //console.log(data.username);
+      
+      if(username == user1 || username == user2) // User is trying to access an inbox they are a participent in
+      {
+        var curUser = username;
+        db.query("SELECT numMessages FROM inbox WHERE user1 = ? AND user2 = ?",inbox,  function(err, data) {
+          if(err)
+          {
+            console.log(err);
+            res.redirect("/login"); // TODO: send error status here
+          }
+          else
+          {
+            numMessages = data[0].numMessages;
+  
+            var recipient, sender;
+            if(curUser == user1) 
+            {
+              recipient = user2; 
+              sender = user1;
+            }
+            else 
+            {
+              recipient = user1;
+              sender = user2;
+            }
+  
+            // insert message into message DB here
+            db.query("INSERT INTO message (user1, user2, messageNo, sender, recipient, content) VALUES (?, ?, ?, ?, ?, ?)", 
+            [user1, user2, numMessages + 1, sender, recipient, messageContent], function(err, data) { // TODO: update so it sends the actual sender and recipient. Not just "username" and "user1"
+                if(err)
+                {
+                  console.log(err);
+                  res.redirect("/login"); // TODO: send error status here
+                }
+                else
+                {
+                  db.query("UPDATE inbox SET numMessages = ? WHERE user1 = ? AND user2 = ?", [numMessages+1, user1, user2], function(err, data)  {
+                    if(err)
+                    {
+                      console.log(err);
+                      res.redirect("/login"); // TODO: send error status here
+                    }
+                  });
+                  // redirect to messages page with user1 and user2 URL parameters intact. 
+                  // Allow users to see reflected change in inbox data
+                  res.redirect("/?user1="+user1+"&user2=" + user2);
+                }
+            });
+          }
+        });
+      }
+      else // User is trying to view other user's inbox
+      {
+        console.log("token or URL parameters is incorrect");
+        res.redirect("/login"); // TODO: send error status here
+      }
+    }
+    catch{
+      console.log("error accessing token data");
+      res.redirect("/login"); // TODO send error status here
+    }
   }
+
+
 });
 
 module.exports = router;
